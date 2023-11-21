8499d4ea6b74be52a32ab00efffe8b11e5c44c46
Gökhan Uysal <guysal20@ku.edu.tr>
2023-11-21 12:46:13 +0200

    Delete asset/sql/project.sql

--- asset/sql/project.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM projects WHERE id='c254bc39-f4f9-4c4b-a38a-10679d0054d7';
-
-SELECT key, value, version, created_at, updated_at  FROM secrets
-WHERE project_id='c254bc39-f4f9-4c4b-a38a-10679d0054d7';
-
-SELECT group_id,user_id FROM group_users
-WHERE group_id='a18eab29-b2e2-4d6f-999e-5bf10c483ee9';
